07eebe6b2c036fecabde09a848466bef843f6ab2
kdavis-mozilla <kdavis@mozilla.com>
2018-10-16 07:11:28 +0200

    Switched to subprocess.run() call

--- kenlm_run.sh
+++ kenlm_run.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 import glob
+import subprocess
 
 # Define directory for arpa files
 arpa_directory = '/snakepit/home/kdavis/v0.3.0/arpa'
@@ -34,12 +35,12 @@ for order in range(min_order, max_order + 1):
             arpa_file = 'verbose_header_order=%d_prune=%d_interpolate_unigrams=%d.arpa' % (order, prune, interpolate_unigrams)
             arpa_path = os.path.join(arpa_directory, arpa_file)
             print('Generating arpa %s' % arpa_path)
-            !build/bin/lmplz --memory 50% --verbose_header --order {order} --prune {prune_arg} --interpolate_unigrams {interpolate_unigrams} --text {librispeech_path} --arpa {arpa_path}
+            subprocess.run('build/bin/lmplz --memory 50% --verbose_header --order %d --prune %s --interpolate_unigrams %d --text %s --arpa %s' % (order, prune_arg, interpolate_unigrams, librispeech_path, arpa_path))
             for counter, limit_vocab_file in enumerate(limit_vocab_files):
                 arpa_file = 'verbose_header_order=%d_prune=%d_interpolate_unigrams=%d_vocab_limit=%d.arpa' % (order, prune, interpolate_unigrams, (10000 + counter*5000))
                 arpa_path = os.path.join(arpa_directory, arpa_file)
                 print('Generating arpa %s' % arpa_path)
-                !build/bin/lmplz --memory 50% --verbose_header --order {order} --prune {prune_arg} --limit_vocab_file {limit_vocab_file} --interpolate_unigrams {interpolate_unigrams} --text {librispeech_path} --arpa {arpa_path}
+                subprocess.run('build/bin/lmplz --memory 50% --verbose_header --order %d --prune %s --limit_vocab_file %s --interpolate_unigrams %d --text %s --arpa %s' % (order, prune_arg, limit_vocab_file, interpolate_unigrams, librispeech_path, arpa_path))
 
 
 # Generate probing models
@@ -53,7 +54,7 @@ for arpa_path in os.listdir(arpa_directory):
             extensionless_arpa_file = os.path.splitext(arpa_file)[0]
             probing_file = (extensionless_arpa_file + '==space_multiplier=%f.probing') % (space_multiplier)
             probing_path = os.path.join(probing_directory, probing_file)
-            !build/bin/build_binary -S 50% -p {space_multiplier} probing {arpa_path} {probing_path}
+            subprocess.run('build/bin/build_binary -S 50% -p %f probing %s %s' % (space_multiplier, arpa_path, probing_path) )
 
 
 # Generate trie models
@@ -65,15 +66,15 @@ for arpa_path in os.listdir(arpa_directory):
         extensionless_arpa_file = os.path.splitext(arpa_file)[0]
         trie_file = extensionless_arpa_file + '.trie'
         trie_path = os.path.join(trie_directory, trie_file)
-        !build/bin/build_binary -S 50% trie {arpa_path} {trie_path}
+        subprocess.run('build/bin/build_binary -S 50% trie %s %s' % (arpa_path, trie_path))
         for bits in range(1, (max_bits + 1)):
             print('Generating trie models quantized to %d bits' % bits)
             for backoff in range(1, bits + 1):
                 print('Generating trie models backed off to %d bits' % backoff)
                 trie_file = (extensionless_arpa_file + '==q=%d_b=%d.trie') % (bits, backoff)
                 trie_path = os.path.join(trie_directory, trie_file)
-                !build/bin/build_binary -S 50% trie -q {bits} -b {backoff} {arpa_path} {trie_path}
+                subprocess.run('build/bin/build_binary -S 50% trie -q %d -b %d %s %s' % (bits, backoff, arpa_path, trie_path))
                 print('Generating trie model with %d array max bits' % array_max_bits)
                 trie_file = (extensionless_arpa_file + '==q=%d_b=%d_a=%d.trie') % (bits, backoff, array_max_bits)
                 trie_path = os.path.join(trie_directory, trie_file)
-                !build/bin/build_binary -S 50% trie -q {bits} -b {backoff} -a {array_max_bits} {arpa_path} {trie_path}
+                subprocess.run('build/bin/build_binary -S 50% trie -q %d -b %d -a %d %s %s' % (bits, backoff, array_max_bits, arpa_path, trie_path))
